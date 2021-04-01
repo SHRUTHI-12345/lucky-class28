0073a9519f9f2d401ccd5d87af5935fb8c50232b
SHRUTHI-12345 <78806048+SHRUTHI-12345@users.noreply.github.com>
2021-04-01 15:04:16 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# lucky-class28
+# BoxModelAngryBird
---
+++ slingshot.js
@@ -0,0 +1,48 @@
+class slingshot {
+    constructor(bodyA,pointB){
+    var options= {
+        bodyA : bodyA,
+        pointB : pointB,
+        length : 10,
+        stiffness : 0.04 // spring effect
+    }
+    this.pointB = pointB;
+    this.sling = Constraint.create(options)
+    World.add(world,this.sling) 
+    }
+
+    fly()
+    {
+     this.sling.bodyA=null   
+    }
+
+
+
+    display()
+    {
+        if(this.sling.bodyA)
+ {
+
+ 
+        var pointA = this.sling.bodyA.position
+        var pointB = this.pointB
+        strokeWeight(7)
+        stroke("brown")
+        line(pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    
